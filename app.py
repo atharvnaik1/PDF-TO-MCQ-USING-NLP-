99c2fcbcd4c0d9010151986a1404184f1207507e
atharv.crypto <69080341+atharvnaik1@users.noreply.github.com>
2023-10-15 18:59:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -138,6 +138,11 @@ st.write(
 
 def main():
     st.title("🧠💬AIQuestor:- ATHARV NAIK \n PDF to Multiple-Choice Questions Generator(Using NLP & AI)")
+     linkedin_text = "LinkedIn"
+    linkedin_link = "https://www.linkedin.com/in/atharv-naik-3175551b4/"
+    
+    st.markdown(f"[{linkedin_text}]({linkedin_link})")
+    
     st.sidebar.header("Sections")
     selected_section = st.sidebar.radio("Select Section",["🏠 EXTRACTED_TEXT", "📝 Generate Questions"])
 
