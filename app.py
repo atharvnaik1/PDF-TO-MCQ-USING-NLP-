ec817e54a2a191c5f8704f583eaf6ec4edbafb93
atharv.crypto <69080341+atharvnaik1@users.noreply.github.com>
2023-10-15 18:41:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ st.write(
 
 def main():
     st.title("🌆💬 AIQuestor:- BY ATHARV NAIK")
-    st.subheader("PDF to Multiple-Choice Questions Generator(Using NLP & AI")
+    st.subheader("PDF to Multiple-Choice Questions Generator(Using NLP & AI)")
     st.sidebar.header("Sections")
     selected_section = st.sidebar.radio("Select Section",["🏠 EXTRACTED_TEXT", "📝 Generate Questions"])
 
